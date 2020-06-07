b2f2100d2ee6b0621936c565ee9cbc83655e2e70
rendanim <mbuvha.rendani@gmail.com>
2020-06-07 08:35:23 +0200

    memory management

--- GRBCM-master/base_lines.m
+++ GRBCM-master/base_lines.m
@@ -4,19 +4,20 @@ rng(100);
 run('../gpml-matlab-v4.2-2018-06-11/startup.m')
 
 
-filelist = {'./uci/airfoil/airfoil.mat';'.uci/kin40k/kin40k.mat';'./uci/energy/energy.mat';'./uci/protein/protein.mat';};
+filelist = {'./uci/concrete/concrete.mat';'./uci/airline/airline.mat';'./uci/airfoil/airfoil.mat';'./uci/kin40k/kin40k.mat';'./uci/energy/energy.mat';'./uci/protein/protein.mat';};
 
 
 %%
 rBCM_nlpd = zeros(4,10);
 GRBCM_nlpd = zeros(4,10);
-NPAE_nlpd = zeros(4,10);
+%NPAE_nlpd = zeros(4,10);
+
 GPoE_nlpd = zeros(4,10);
 
 
 rBCM_RMSE = zeros(4,10);
 GRBCM_RMSE = zeros(4,10);
-NPAE_RMSE = zeros(4,10);
+%NPAE_RMSE = zeros(4,10);
 GPoE_RMSE = zeros(4,10);
 
 
@@ -32,7 +33,7 @@ for j= 1:4
 
     cv0 = cvpartition(n, 'k', 10);
     sf2 = 1 ; ell = 1 ; sn2 = 0.1 ; 
-    partitionCriterion = 'kmeans' ; % 'random', 'kmeans'
+    partitionCriterion = 'random' ; % 'random', 'kmeans'
 
     % train
     opts.Xnorm = 'Y' ; opts.Ynorm = 'Y' ;
@@ -40,7 +41,8 @@ for j= 1:4
     opts.ell = ell ; opts.sf2 = sf2 ; opts.sn2 = sn2 ;
     opts.meanfunc = []; opts.covfunc = @covSEard; opts.likfunc = @likGauss; opts.inffunc = @infGaussLik ;
     opts.numOptFC = 100;
-    
+    batch_size = 1000;
+
     for i=1: cv0.NumTestSets
         trainIdx = cv0.training(i);
         testIdx = cv0.test(i);
@@ -50,17 +52,42 @@ for j= 1:4
         yt = data(testIdx, end);
         [models,t_dGP_train] = aggregation_train(x,y,opts) ;
          % PoE, GPoE, BCM, RBCM, GRBCM, NPAE
-        [mu_GPoE,s2_GPoE,t_GPoE_predict] = aggregation_predict(xt,models,'GPoE') ;
-        [mu_GRBCM,s2_GRBCM,t_GRBCM_predict] = aggregation_predict(xt,models,'GRBCM') ;
-        [mu_RBCM,s2_RBCM,t_RBCM_predict] = aggregation_predict(xt,models,'RBCM') ;
-        [mu_NPAE,s2_NPAE,t_NPAE_predict] = aggregation_predict(xt,models,'NPAE') ;
-        rBCM_RMSE(j,i) = sqrt(mean((yt- mu_RBCM).^2));
+        %[mu_GPoE,s2_GPoE,t_GPoE_predict] = aggregation_predict(xt,models,'GPoE') ;
+        num_batches = ceil(size(xt,1)/batch_size);
+        if size(xt,1)> batch_size
+            for k=1:num_batches
+                [mu_GRBCM_k,s2_GRBCM_k,t] = aggregation_predict(xt((k-1)*batch_size+1:(k)*batch_size,:),models,'GRBCM');
+                [mu_GPoE_k,s2_GPoE_k,t] = aggregation_predict(xt((k-1)*batch_size+1:(k)*batch_size,:),models,'GPoE');
+                %[mu_NPAE_k,s2_NPAE_k,t] = aggregation_predict(xt((k-1)*batch_size+1:(k)*batch_size,:),models,'NPAE');
+
+                if k==1
+                    mu_GRBCM = mu_GRBCM_k;
+                    s2_GRBCM = s2_GRBCM_k;
+                    mu_GPoE = mu_GPoE_k;
+                    s2_GPoE = s2_GPoE_k;
+                 %   mu_NPAE = mu_NPAE_k;
+                  %  s2_NPAE = s2_NPAE_k;
+                else
+                     mu_GRBCM = vertcat(mu_GRBCM, mu_GRBCM_k);
+                     s2_GRBCM = vertcat(s2_GRBCM,s2_GRBCM_k);
+                      mu_GPoE = vertcat(mu_GPoE, mu_GPoE_k);
+                     s2_GPoE = vertcat(s2_GRBCM,s2_GPoE_k);
+                   %   mu_NPAE = vertcat(mu_NPAE, mu_NPAE_k);
+                    % s2_NPAE = vertcat(s2_NPAE,s2_NPAE_k);
+                end
+            end
+        else
+            [mu_GRBCM,s2_GRBCM,t] = aggregation_predict(xt,models,'GRBCM');
+            [mu_GPoE,s2_GPoE,t] = aggregation_predict(xt,models,'GPoE');
+               
+        end
+        %rBCM_RMSE(j,i) = sqrt(mean((yt- mu_RBCM).^2));
         GRBCM_RMSE(j,i) =sqrt(mean((yt- mu_GRBCM).^2));
-        NPAE_RMSE(j,i) = sqrt(mean((yt -mu_NPAE).^2));
+        %NPAE_RMSE(j,i) = sqrt(mean((yt -mu_NPAE).^2));
         GPoE_RMSE(j,i) = sqrt(mean((yt -mu_GPoE).^2));
-        rBCM_nlpd(j,i) = nlpd(yt,mu_RBCM, s2_RBCM);
+        %rBCM_nlpd(j,i) = nlpd(yt,mu_RBCM, s2_RBCM);
         GPoE_nlpd(j,i) = nlpd(yt,mu_GPoE, s2_GPoE);
-        NPAE_nlpd(j,i) = nlpd(yt,mu_NPAE, s2_NPAE);
+        %NPAE_nlpd(j,i) = nlpd(yt,mu_NPAE, s2_NPAE);
         GRBCM_nlpd(j,i) = nlpd(yt,mu_GRBCM, s2_GRBCM);    
     end
 end
@@ -70,11 +97,11 @@ end
 
 
 %%
-nlpds =[mean(GRBCM_nlpd), mean(GPoE_nlpd), mean(NPAE_nlpd)];
-rmses =[mean(GRBCM_RMSE), mean(GPoE_RMSE), mean(NPAE_RMSE)];
-X = categorical({'GRBCM','GPOE','NPAE'});
-X = reordercats(X,{'GRBCM','GPOE','NPAE'});
+%nlpds =[mean(GRBCM_nlpd), mean(GPoE_nlpd), mean(NPAE_nlpd)];
+%rmses =[mean(GRBCM_RMSE), mean(GPoE_RMSE), mean(NPAE_RMSE)];
+X = categorical({'GRBCM','GPOE'});
+X = reordercats(X,{'GRBCM','GPOE'});
 
-save nlpds.mat GRBCM_nlpd NPAE_nlpd GPoE_nlpd
-save rmse.mat GRBCM_RMSE NPAE_RMSE GPoE_RMSE X
+save nlpds.mat GRBCM_nlpd GPoE_nlpd
+save rmse.mat GRBCM_RMSE GPoE_RMSE X
 
